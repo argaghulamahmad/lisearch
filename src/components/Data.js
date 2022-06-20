74e87493fb89464a9e65fb3537049dde570c2179
Arga Ghulam Ahmad <arga.ahmad@kargo.tech>
2022-06-20 14:37:46 +0700

    update

--- src/components/Data.js
+++ src/components/Data.js
@@ -31,49 +31,7 @@ const Data = () => {
                             dataIndex: 'fullName',
                             key: 'fullName',
                             render: text => <a>{text}</a>,
-                        },
-
-
-                        // {
-                        //     title: 'Age',
-                        //     dataIndex: 'age',
-                        //     key: 'age',
-                        // },
-                        // {
-                        //     title: 'Address',
-                        //     dataIndex: 'address',
-                        //     key: 'address',
-                        // },
-                        // {
-                        //     title: 'Tags',
-                        //     key: 'tags',
-                        //     dataIndex: 'tags',
-                        //     render: (_, {tags}) => (
-                        //         <>
-                        //             {tags.map(tag => {
-                        //                 let color = tag.length > 5 ? 'geekblue' : 'green';
-                        //                 if (tag === 'loser') {
-                        //                     color = 'volcano';
-                        //                 }
-                        //                 return (
-                        //                     <Tag color={color} key={tag}>
-                        //                         {tag.toUpperCase()}
-                        //                     </Tag>
-                        //                 );
-                        //             })}
-                        //         </>
-                        //     ),
-                        // },
-                        // {
-                        //     title: 'Action',
-                        //     key: 'action',
-                        //     render: (_, record) => (
-                        //         <Space size="middle">
-                        //             <a>Invite {record.name}</a>
-                        //             <a>Delete</a>
-                        //         </Space>
-                        //     ),
-                        // }
+                        }
                     ]
                 }
                dataSource={connections}/>
