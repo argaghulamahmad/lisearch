bc4b45110014ef1e2dd3d3bbf4c67d00d1aaf90b
Arga Ghulam Ahmad <arga.ahmad@kargo.tech>
2022-09-06 12:07:02 +0700

    render copy button if value is not empty

--- src/components/CopyToClipboard.js
+++ src/components/CopyToClipboard.js
@@ -3,11 +3,12 @@ import {Button, notification} from "antd";
 import {CopyOutlined} from "@ant-design/icons";
 
 export const CopyToClipboard = ({value}) => (
-    <Button type="text" icon={<CopyOutlined/>} onClick={() => {
-        navigator.clipboard.writeText(value);
-        notification.success({
-            message: "Copied to clipboard",
-            description: `${value} copied to clipboard!`,
-        });
-    }} size="small"/>
+    value !== undefined && value !== "" ?
+        <Button type="text" icon={<CopyOutlined/>} onClick={() => {
+            navigator.clipboard.writeText(value);
+            notification.success({
+                message: "Copied to clipboard",
+                description: `${value} copied to clipboard!`,
+            });
+        }} size="small"/> : <div/>
 );
