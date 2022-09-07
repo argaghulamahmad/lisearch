9839ddb63b722da0e18fdc363ef5aee02562550c
Arga Ghulam Ahmad <arga.ahmad@kargo.tech>
2022-09-07 21:11:32 +0700

    fix Stats.js

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -7,44 +7,45 @@ export const Stats = () => {
     const history = useHistory();
 
     return (
-        JSON.parse(localStorage.getItem('connections')) ? <div style={{width: "1200px"}}>
-            <Row gutter={16}>
-                <Col span={8}>
-                    <Card hoverable={true}
-                          onClick={() => {
-                              history.push('/connections');
-                          }}>
-                        <Statistic
-                            title="Connections"
-                            value={JSON.parse(localStorage.getItem('connections')).length}
-                        />
-                    </Card>
-                </Col>
-                <Col span={8}>
-                    <Card hoverable={true}
-                          onClick={() => {
-                              history.push('/companies');
-                          }}>
-                        <Statistic
-                            title="Companies"
-                            value={JSON.parse(localStorage.getItem('companies')).length}
-                        />
-                    </Card>
-                </Col>
-                <Col span={8}>
-                    <Card hoverable={true}
-                          onClick={() => {
-                              history.push('/positions');
-                          }}>
-                        <Statistic
-                            title="Positions"
-                            value={JSON.parse(localStorage.getItem('positions')).length}
-                        />
-                    </Card>
-                </Col>
-            </Row>
-        </div> : <div style={{width: "1200px"}}>
-            <Empty/>
-        </div>
+        JSON.parse(localStorage.getItem('connections')) && JSON.parse(localStorage.getItem('companies')) && JSON.parse(localStorage.getItem('positions')) ?
+            <div style={{width: "1200px"}}>
+                <Row gutter={16}>
+                    <Col span={8}>
+                        <Card hoverable={true}
+                              onClick={() => {
+                                  history.push('/connections');
+                              }}>
+                            <Statistic
+                                title="Connections"
+                                value={JSON.parse(localStorage.getItem('connections')).length}
+                            />
+                        </Card>
+                    </Col>
+                    <Col span={8}>
+                        <Card hoverable={true}
+                              onClick={() => {
+                                  history.push('/companies');
+                              }}>
+                            <Statistic
+                                title="Companies"
+                                value={JSON.parse(localStorage.getItem('companies')).length}
+                            />
+                        </Card>
+                    </Col>
+                    <Col span={8}>
+                        <Card hoverable={true}
+                              onClick={() => {
+                                  history.push('/positions');
+                              }}>
+                            <Statistic
+                                title="Positions"
+                                value={JSON.parse(localStorage.getItem('positions')).length}
+                            />
+                        </Card>
+                    </Col>
+                </Row>
+            </div> : <div style={{width: "1200px"}}>
+                <Empty/>
+            </div>
     );
 };
