b12a74f643b78b73aa48842edea446b336c3fe66
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2023-08-13 22:12:24 +0700

    Update Config.js

--- src/pages/Config.js
+++ src/pages/Config.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Button, Divider, Form, InputNumber, Space, notification } from 'antd';
+import {Button, Divider, Form, InputNumber, Space, notification} from 'antd';
 
 const Config = () => {
     const handleConfigFormSubmit = (configValues) => {
         localStorage.setItem('config', JSON.stringify(configValues));
         notification.success({
-            message: 'Success',
-            description: 'Config updated!',
+            message: 'Success', description: 'Config updated!',
         });
     };
 
@@ -16,27 +15,24 @@ const Config = () => {
         localStorage.removeItem('visitedPositions');
 
         notification.success({
-            message: 'Success',
-            description: 'All visited random profiles data has been reset!',
+            message: 'Success', description: 'All visited random profiles data has been reset!',
         });
     };
 
     const handleResetAllData = () => {
         localStorage.clear();
         notification.success({
-            message: 'Success',
-            description: 'All data cleared!',
+            message: 'Success', description: 'All data cleared!',
         });
     };
 
-    return (
-        <div style={{ width: "1200px" }}>
-            <div>
+    return (<div style={{width: "1200px"}}>
+            <div style={{textAlign: "left",}}>
                 Customize your settings here!
             </div>
             <div>
                 <Divider orientation="left">Setup Config</Divider>
-                <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
+                <Space direction="vertical" size="middle" style={{display: 'flex'}}>
                     <Form
                         name="configForm"
                         initialValues={{
@@ -46,29 +42,28 @@ const Config = () => {
                     >
                         <Form.Item label="Feel Lucky Generator Counts">
                             <Form.Item name="feelLuckyGeneratorCounts" noStyle>
-                                <InputNumber min={1} max={20} />
+                                <InputNumber min={1} max={20}/>
                             </Form.Item>
                             <span className="ant-form-text"> record</span>
                         </Form.Item>
                         <Form.Item>
-                            <Button type="primary" style={{ width: '100%' }} htmlType="submit">
+                            <Button type="primary" style={{width: '100%'}} htmlType="submit">
                                 Save Config
                             </Button>
                         </Form.Item>
                     </Form>
                 </Space>
                 <Divider orientation="left">Reset Data</Divider>
-                <Space direction="vertical" size="small" style={{ display: 'flex' }}>
-                    <Button type="danger" style={{ width: '100%' }} onClick={handleResetConnections}>
+                <Space direction="vertical" size="small" style={{display: 'flex'}}>
+                    <Button type="danger" style={{width: '100%'}} onClick={handleResetConnections}>
                         Reset all feel lucky connections generator
                     </Button>
-                    <Button type="danger" style={{ width: '100%' }} onClick={handleResetAllData}>
+                    <Button type="danger" style={{width: '100%'}} onClick={handleResetAllData}>
                         Reset all data
                     </Button>
                 </Space>
             </div>
-        </div>
-    );
+        </div>);
 }
 
 export default Config;
