f528fb66d1614f5fc96d086d6323ff94d4365853
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2023-08-13 21:47:01 +0700

    Optimize and enhance App.js

--- src/App.js
+++ src/App.js
@@ -1,59 +1,55 @@
+import React from "react";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import { Divider, Menu, Space } from "antd";
 import Connections from "./pages/Connections";
 import Companies from "./pages/Companies";
-import Uploader from './pages/Uploader';
-import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
-import {Divider, Menu, Space} from "antd";
+import Uploader from "./pages/Uploader";
 import Positions from "./pages/Positions";
 import Stats from "./components/Stats";
 import Config from "./pages/Config";
 
 function App() {
     return (
-        <div className="App" style={{textAlign: "center"}}>
+        <div className="App" style={{ textAlign: "center" }}>
             <Router>
-                <div className="App">
-                    <div className="AppContent">
-                        <h1 style={{padding: "1% 3% 0 3%"}}>Li Search</h1>
-                        <Divider orientation="left" plain style={{marginBottom: 0}}>
-                        </Divider>
-                        <Space size="middle" align="vertical">
-                            <Menu mode="vertical">
-                                <Menu.Item key="home">
-                                    <Link to="/connections">Home</Link>
-                                </Menu.Item>
-                                <Menu.Item key="uploadFile">
-                                    <Link to="/upload">Upload</Link>
-                                </Menu.Item>
-                                <Menu.Item key="config">
-                                    <Link to="/config">Config</Link>
-                                </Menu.Item>
-                            </Menu>
+                <div className="AppContent">
+                    <h1 style={{ padding: "1% 3% 0 3%" }}>Li Search</h1>
+                    <Divider orientation="left" plain style={{ marginBottom: 0 }} />
 
-                            <Switch>
-                                <Route exact path="/">
-                                    <Stats/>
-                                </Route>
-                                <Route exact path="/connections">
-                                    <Stats/>
-                                    <Connections/>
-                                </Route>
-                                <Route path="/positions">
-                                    <Stats/>
-                                    <Positions/>
-                                </Route>
-                                <Route exact path="/companies">
-                                    <Stats/>
-                                    <Companies/>
-                                </Route>
-                                <Route exact path="/upload">
-                                    <Uploader/>
-                                </Route>
-                                <Route exact path="/config">
-                                    <Config/>
-                                </Route>
-                            </Switch>
-                        </Space>
-                    </div>
+                    <Space size="middle" align="vertical">
+                        <Menu mode="vertical">
+                            <Menu.Item key="home">
+                                <Link to="/connections">Home</Link>
+                            </Menu.Item>
+                            <Menu.Item key="uploadFile">
+                                <Link to="/upload">Upload</Link>
+                            </Menu.Item>
+                            <Menu.Item key="config">
+                                <Link to="/config">Config</Link>
+                            </Menu.Item>
+                        </Menu>
+
+                        <Switch>
+                            <Route exact path={["/", "/connections", "/positions", "/companies"]}>
+                                <Stats />
+                            </Route>
+                            <Route exact path="/connections">
+                                <Connections />
+                            </Route>
+                            <Route exact path="/positions">
+                                <Positions />
+                            </Route>
+                            <Route exact path="/companies">
+                                <Companies />
+                            </Route>
+                            <Route exact path="/upload">
+                                <Uploader />
+                            </Route>
+                            <Route exact path="/config">
+                                <Config />
+                            </Route>
+                        </Switch>
+                    </Space>
                 </div>
             </Router>
         </div>
