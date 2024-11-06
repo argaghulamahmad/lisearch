8b974bfbea92504b544a2e9b6209fc564d39826b
Arga Ghulam Ahmad <arga.ghulam@allofresh.id>
2024-11-06 17:31:34 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Divider, Space, Tabs } from "antd";
+import { Divider, Space, Tabs, Typography } from "antd";
 import Connections from "./pages/Connections";
 import Companies from "./pages/Companies";
 import Uploader from "./pages/Uploader";
@@ -9,38 +8,35 @@ import Stats from "./components/Stats";
 import Config from "./pages/Config";
 
 const { TabPane } = Tabs;
+const { Title } = Typography;
 
 function App() {
     return (
         <div className="App" style={{ textAlign: "center", width: "100%", padding: "0 20px" }}>
-            <Router>
-                <div className="AppContent" style={{ width: "100%" }}>
-                    <h1 style={{ padding: "1% 0" }}>Li Search</h1>
-                    <Divider orientation="left" plain style={{ marginBottom: 0 }} />
-                    <Space size="middle" align="vertical" style={{ width: "100%" }}>
-                        <Tabs defaultActiveKey="1" style={{ width: "100%" }}>
-                            <TabPane tab="Home" key="1">
-                                <Stats />
-                                <Connections />
-                            </TabPane>
-                            <TabPane tab="Companies" key="2">
-                                <Stats />
-                                <Companies />
-                            </TabPane>
-                            <TabPane tab="Positions" key="3">
-                                <Stats />
-                                <Positions />
-                            </TabPane>
-                            <TabPane tab="Upload" key="4">
-                                <Uploader />
-                            </TabPane>
-                            <TabPane tab="Config" key="5">
-                                <Config />
-                            </TabPane>
-                        </Tabs>
-                    </Space>
-                </div>
-            </Router>
+            <Space direction="vertical" style={{ width: "100%" }}>
+                <Title level={1} style={{ margin: "1% 0" }}>Li Search</Title>
+                <Stats />
+                <Divider orientation="left" plain style={{ marginBottom: 0 }} />
+                <Space size="middle" align="center" style={{ width: "100%" }}>
+                    <Tabs defaultActiveKey="1" style={{ width: "100%" }}>
+                        <TabPane tab="Home" key="1">
+                            <Connections />
+                        </TabPane>
+                        <TabPane tab="Companies" key="2">
+                            <Companies />
+                        </TabPane>
+                        <TabPane tab="Positions" key="3">
+                            <Positions />
+                        </TabPane>
+                        <TabPane tab="Upload" key="4">
+                            <Uploader />
+                        </TabPane>
+                        <TabPane tab="Config" key="5">
+                            <Config />
+                        </TabPane>
+                    </Tabs>
+                </Space>
+            </Space>
         </div>
     );
 }
--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,49 +1,48 @@
 import React, { useEffect, useState } from "react";
 import { Card, Col, Row, Statistic } from "antd";
-import { useHistory } from "react-router-dom";
 import db from "../db";
 
 const Stats = () => {
-    const history = useHistory();
-
-    const [connectionsCount, setConnectionsCount] = useState(0);
-    const [companiesCount, setCompaniesCount] = useState(0);
-    const [positionsCount, setPositionsCount] = useState(0);
+    const [counts, setCounts] = useState({
+        connectionsCount: 0,
+        companiesCount: 0,
+        positionsCount: 0,
+    });
 
     useEffect(() => {
-        async function fetchCounts() {
+        const fetchCounts = async () => {
             const [connections, companies, positions] = await Promise.all([
                 db.connections.toArray(),
                 db.companies.toArray(),
                 db.positions.toArray()
             ]);
 
-            setConnectionsCount(connections.length);
-            setCompaniesCount(companies.length);
-            setPositionsCount(positions.length);
+            setCounts({
+                connectionsCount: connections.length,
+                companiesCount: companies.length,
+                positionsCount: positions.length,
+            });
 
             console.info("Connections", connections.length);
-        }
+        };
 
         fetchCounts().then(r => console.info(r));
     }, []);
 
-    const renderStatisticCard = (title, count, route) => {
-        return (
-            <Col span={8}>
-                <Card hoverable onClick={() => history.push(route)}>
-                    <Statistic title={title} value={count} />
-                </Card>
-            </Col>
-        );
-    };
+    const renderStatisticCard = (title, count) => (
+        <Col span={8}>
+            <Card hoverable>
+                <Statistic title={title} value={count} />
+            </Card>
+        </Col>
+    );
 
     return (
         <div style={{ width: "1200px" }}>
             <Row gutter={16}>
-                {renderStatisticCard("Connections", connectionsCount, '/connections')}
-                {renderStatisticCard("Companies", companiesCount, '/companies')}
-                {renderStatisticCard("Positions", positionsCount, '/positions')}
+                {renderStatisticCard("Connections", counts.connectionsCount)}
+                {renderStatisticCard("Companies", counts.companiesCount)}
+                {renderStatisticCard("Positions", counts.positionsCount)}
             </Row>
         </div>
     );
